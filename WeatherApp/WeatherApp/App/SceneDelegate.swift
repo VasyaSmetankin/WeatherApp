71c2f04c594578b30ce7d22e88b533f73f10251d
VasyaSmetankin <meemkook@icloud.com>
2024-07-20 01:11:02 +0300

    commit before everything gets fucked up absolutely

--- WeatherApp/WeatherApp/App/SceneDelegate.swift
+++ WeatherApp/WeatherApp/App/SceneDelegate.swift
@@ -10,26 +10,14 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-        
         let newInitialViewController = ViewController()
-        
-        
         window = UIWindow(windowScene: windowScene)
-        
-        
         window?.rootViewController = newInitialViewController
-        
-        
         window?.makeKeyAndVisible()
-
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
